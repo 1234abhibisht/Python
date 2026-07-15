0a4d866cd62bb77e09fcdd2e42c8a132f429af1c
1234abhibisht <abhibisht129@gmail.com>
2026-07-15 21:01:36 +0530

    created 02_CollectionsMethods.py file

--- 06_CollectionsInPython/02_CollectionsMethods.py
+++ 06_CollectionsInPython/02_CollectionsMethods.py
@@ -9,3 +9,16 @@ print(a3_counter.most_common())  # -> [('a', 4), ('b', 2), ('c', 1)]
 
 # namedtuple ->
 from collections import namedtuple
+employee = namedtuple("employee", ["id", "name", "department"])  # created a namedtuple employee having name "employee"
+rec1 = employee(123, "abc", "IT")
+# ._asdict method - return namedtuple as an ordered dictionary having field name as key and field element as values
+print(rec1._asdict())
+# ._fields - return field names as a tuple
+print(rec1._fields)
+# ._replace - create new instance with changed field values
+rec2 = rec1._replace(name="def")
+print(rec2)  # -> employee(id=123, name='def', department='IT')
+# ._make - create a new instance from iterables
+li = [456, "john", "backend"]
+rec3 = employee._make(li)
+print(rec3)  # -> employee(id=456, name='john', department='backend')
